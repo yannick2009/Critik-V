fd086878e864aebbcc322c7aea98dc4956e88532
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2023-11-22 01:47:39 +0100

    [client] - resume-guide page made

--- client/src/pages/ResumeGuide.tsx
+++ client/src/pages/ResumeGuide.tsx
@@ -1,5 +1,17 @@
 import "./styles/ResumeGuide.scss";
+import img from "@assets/img/coffre-fort.png";
 
-export default function ResumeGuide() {
-  return <div>ResumeGuide</div>;
+const title: string = "Guide du CV";
+const text: string =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga fugiat voluptatem qui? Laborum commodi facilis ipsa expedita voluptas voluptate obcaecati, ducimus numquam, amet quasi rerum alias, nam animi eos dolore.";
+
+export default function ResumeGuide(): JSX.Element {
+  return (
+    <section id="resumeGuide">
+      <img src={img} alt="tresor-image" />
+      <h3>{title}</h3>
+      <p>{text}</p>
+      <button>Telecharger le PDF</button>
+    </section>
+  );
 }
