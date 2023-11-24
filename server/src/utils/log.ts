95b4edfc7d5707fd6948b6ab9e6399133aaecd4c
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2023-11-24 01:43:15 +0100

    [backend] - setup the utils folder

---
+++ server/src/utils/log.ts
@@ -0,0 +1,7 @@
+import chalk, { Chalk } from "chalk";
+
+export const chalk_succes: Chalk = chalk.bgHex("#02c39a").bold.hex("#ffff");
+export const chalk_error: Chalk = chalk.bgHex("#ef233c").bold.hex("#ffff");
+export const chalk_warning: Chalk = chalk.bgHex("#f39200").bold.hex("#ffff");
+export const chalk_db: Chalk = chalk.bgHex("#8338ec").bold.hex("#ffff");
+export const chalk_mongo: Chalk = chalk.bgHex("#f72585").bold.hex("#ffff");
